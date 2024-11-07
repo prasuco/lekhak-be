bc3699856dfe6ce05cb209cddaf8bbcaa65aa90c
prabincankod <prabinsubedi2016@gmail.com>
2024-11-07 18:04:51 +0545

    setup: prisma

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from "express";
 
+import dotenv from "dotenv";
 const app = express();
 
+dotenv.config();
 app.get("/", (req, res) => {
   res.json({ success: true });
 });
-const PORT = process.env.PORT || 4321
-
+const PORT = process.env.PORT || 4321;
 
 app.listen(PORT, () =>
   console.log(`server running in http://localhost:${PORT}`)
